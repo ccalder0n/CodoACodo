949fa8754595ce0a349057da35f6e6078b31d75c
danibarboza2014 <38190347+danibarboza2014@users.noreply.github.com>
2024-06-27 14:22:40 -0300

    Update tienda.js

--- static/js/tienda.js
+++ static/js/tienda.js
@@ -89,7 +89,8 @@ let noticiasJson = [
 
 
 
-  const URL = "http://127.0.0.1:5000/"
+//const URL = "http://127.0.0.1:5000/"
+  const URL = "https://santicasalis.pythonanywhere.com/"
   
   fetch(URL + 'productos')
           .then(function (response) {
